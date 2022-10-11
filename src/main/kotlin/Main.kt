93f4b1ce794e2cd38d3d7b7c280dfd436bbefa22
Phazerous <nphazerous@gmail.com>
2022-10-11 11:47:35 +0600

    Lab 1 10 Binary tree search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class TreeNode(val value: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Tree {
+    var height: Int = 0
+    var root: TreeNode? = null
+
+    fun insert(n: Int) {
+        height = 1
+        root = insert(root, n, 1)
+    }
+
+    fun insert(node: TreeNode?, n: Int, level: Int): TreeNode {
+        height = if (level > height) level else height
+
+        if (node == null) return TreeNode(n)
+
+        if (node.value == n) return node
+
+        if (node.value > n) {
+            node.right = insert(node.right, n, level + 1)
+        } else {
+            node.left = insert(node.left, n, level + 1)
+        }
+
+        return node
+    }
+}
+
+fun main() {
+    val tree = Tree()
+    var n: Int
+
+    while (true) {
+        n = readln().toInt()
+
+        if (n == 0) break
+
+        tree.insert(n)
+    }
+
+    println("Height of the tree: " + tree.height)
 }
